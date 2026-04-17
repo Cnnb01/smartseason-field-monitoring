9584168334fadd5eda15f8eee4d5ee54281ba648
Cnnb01 <nyamambanyaguch@gmail.com>
2026-04-17 22:27:19 +0300

    registering and testing out models on the admin dashboard

--- Backend/fields/admin.py
+++ Backend/fields/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Field, FieldUpdate
 
 # Register your models here.
+# show the User model
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'role', 'is_staff')
+
+# shows the Field model and includes your custom status property
+@admin.register(Field)
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('name', 'crop_type', 'current_stage', 'status', 'assigned_agent')
+    list_filter = ('current_stage', 'assigned_agent')
+admin.site.register(FieldUpdate)
