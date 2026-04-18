1c8cd6fb1817f0a7cfc2a67aa1156412c9a01d03
Cnnb01 <nyamambanyaguch@gmail.com>
2026-04-18 17:53:51 +0300

    Add postgres driver psycopg

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -101,10 +101,6 @@ DATABASES = {
         default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600
     )
-    # 'default': dj_database_url.config(
-    #     default='sqlite:///db.sqlite3',
-    #     conn_max_age=600
-    # )
 }
 
 
