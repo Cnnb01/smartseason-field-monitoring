0a2846e11f114697242cb997f25884a32cac6ec7
Cnnb01 <nyamambanyaguch@gmail.com>
2026-04-18 17:47:10 +0300

    Add postgres driver psycopg

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -148,6 +148,6 @@ AUTH_USER_MODEL = 'fields.User'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://smartseason-field-monitoring.vercel.app/login"
+    "https://smartseason-field-monitoring.vercel.app/"
     "http://127.0.0.1:5173",
 ]
